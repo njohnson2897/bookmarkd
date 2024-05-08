9383c7630765326445810cdd71c10066c46f6b5d
Nate Johnson <njohnson2897@gmail.com>
2024-05-08 08:41:05 -0500

    adds multiple pages and routing from main.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className='bg-secondary text-center fs-3'>
+       <p>Made by: Alexa Chavez, Robin Langton, Domas Dargis, and Nate Johnson</p>
+        </div>
+    );
+}
---
+++ client/src/pages/User.jsx
@@ -0,0 +1,8 @@
+export default function User() {
+  
+    return (
+      <div>
+        <h1>User Page</h1>
+      </div>
+    );
+  }
