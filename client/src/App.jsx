b8d1a98baf225e7f9e28448751628c516b479693
Robin <rrlangton@gmail.com>
2024-05-08 18:15:20 -0500

    footer feature CSS implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Footer from "./components/Footer.jsx";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 
-
 function App() {
   return (
   <>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,101 @@
-export default function Footer() {
+function Footer() {
     return (
-        <div className='bg-secondary text-center fs-3'>
-       <p>Made by: Alexa Chavez, Robin Langton, Domas Dargis, and Nate Johnson</p>
+        <section className="info_section ">
+    <div className="info_container layout_padding-top">
+      <div className="container">
+        <div className="info_top">
+          <div className="info_logo">
+            <img src="images/logo.png" alt="" />
+            <span>
+              Bookmarkd\
+            </span>
+          </div>
+          <span>Enjoying the App?</span>
+          <span>Share On Social Media!</span>
+          <div className="social_box">
+            <a href="https://www.facebook.com/" target="_blank">
+              <img src="images/fb.png" alt="facebook logo"/>
+            </a>
+            <a href="https://twitter.com/" target="_blank">
+              <img src="images/twitter.png" alt="twitter logo"/>
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank">
+              <img src="images/linkedin.png" alt="linkedin logo"/>
+            </a>
+            <a href="https://www.instagram.com/" target="_blank">
+              <img src="images/instagram.png" alt="instagram logo"/>
+            </a>
+          </div>
         </div>
+
+        <div className="info_main">
+          <div className="row">
+            <div className="col-md-3 col-lg-2">
+              <div className="info_link-box">
+                <h5>
+                  Links
+                </h5>
+                <ul>
+                  <li className=" active">
+                    <a className="" href="index.html">Home <span className="sr-only">(current)</span></a>
+                  </li>
+                  <li className="">
+                    <a className="" href="users.html">Sign in</a>
+                  </li>
+                  <li className="">
+                    <a className="" href="books.html">Browse Books</a>
+                  </li>
+                  <li className="">
+                    <a className="" href="bookclubs.html">Book Clubs</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3 ">
+              <h5>
+                About Bookmarkd
+              </h5>
+              <p>
+                The perfect place to read & write book reviews, curate personalized book lists, and join book clubs!
+              </p>
+            </div>
+
+            <div className="col-md-3 col-lg-2 offset-lg-1">
+              <h5>
+                <div className="social_box">
+                  <a href="https://github.com/njohnson2897/bookmarkd">
+                    <img src="images/gh.png" alt="githublogo"/>Github
+                  </a>
+                  </div>
+              </h5>
+              <h6>Alexa Aguinada, Domas Dar, Nate Johnson, & Robin Langton</h6>
+            </div>
+
+            <div className="col-md-3  offset-lg-1">
+              <div className="info_form ">
+                <h5>
+                  Contact Us
+                </h5>
+                <div class="form">
+                  <form>
+                      <div class="form-group">
+                          <emailarea type="email" className="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" required></emailarea>
+                      </div>
+                      <div className="form-group">
+                          <textarea name="contact-message" id="contact-message" cols="30" rows="2" className="form-control form-control-sm" placeholder="Message"></textarea>
+                      <button type="submit" className="btn btn-primary btn-block btn-sm mt-3">Send Message</button>
+                    </div>
+                  </form>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
     );
 }
+
+export default Footer;
