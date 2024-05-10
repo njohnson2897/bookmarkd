607be7774595ffbbd2936330e013743378d3c555
Expired Milk <domasdar2@gmail.com>
2024-05-10 11:41:12 -0500

    Added all necessary resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,21 +30,58 @@ const resolvers = {
                 { $addToSet: { books: { book, status, favorite }}}
             )
         },
-        addReview: async (parent, {book_id, user_id, stars, title, description}) => {
-            const review = await Review.create({ book_id, user_id, stars, title, description });
+        addReview: async (parent, {book, user, stars, title, description}) => {
+            const review = await Review.create({ book, user, stars, title, description });
             await User.findOneAndUpdate(
-                { _id: user_id },
+                { _id: user },
                 { $addToSet: { reviews: review._id } }
             );
             await Book.findOneAndUpdate(
-                { _id: book_id },
+                { _id: book },
                 { $addToSet: { reviews: review._id } }
             );
             return review;
         },
         addClub: async (parent, {name, owner}) => {
-            
-        }
+            return Club.create({ name, owner });
+        },
+        deleteReview: async(parent, { reviewId }) => {
+            const reviewData = await Review.findOne({ _id: reviewId })
+            const updatedBook = await Book.findOneAndUpdate(
+                { _id: reviewData.book },
+                { $pull: { reviews: reviewId  } },
+                { new : true}
+            );
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: reviewData.user },
+                { $pull: { reviews: reviewId  } },
+                {new: true}
+            )
+            return await Review.findOneAndDelete( { _id: reviewId } );
+        },
+        deleteClub: async (parent, { clubId }) => {
+            const clubData = await Club.findOne({ _id: clubId });
+            await User.findOneAndUpdate(
+                { _id: clubData.owner },
+                { $pull: { clubs: clubId } }
+            );
+            if(clubData.members){
+                const noMembers = await clubData.owner.map((member) => {
+                    User.findOneAndUpdate(
+                        { _id: member },
+                        { $pull: { clubs: clubId } }
+                    );
+                })
+            }
+            return await Club.findOneAndDelete( { _id: clubId } );
+        },
+        removeUserBook: async (parent, { bookId, userId }) => {
+            return await User.findOneAndUpdate(
+                { _id: userId},
+                { $pull: { books: { book: bookId }}},
+                { multi: true }
+            );
+        },
     }
 };
 
