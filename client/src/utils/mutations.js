96d512c7d6c793085365dc81a891be658e8f4df0
Nate Johnson <njohnson2897@gmail.com>
2024-05-10 10:54:29 -0500

    connects log in and sign up functionality to front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,3 +10,15 @@ export const LOGIN_USER = gql`
             }
         }
     }`
+
+export const ADD_USER = gql`
+mutation Mutation($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }`
